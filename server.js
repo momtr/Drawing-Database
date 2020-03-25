8eae98e664e8a52dc5d222e8a88c6b4fcd6a5bb8
moritzmitterdorfer <mitterdorfer.moritz@gmail.com>
2020-03-26 00:56:37 +0100

    init

--- server.js
+++ server.js
@@ -1,52 +1,61 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
+const Datastore = require('nedb');
+const fs = require('fs');
+
+// set up database
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+// get words from text file
+let words;
+fs.readFile('words.txt', 'utf8', (err, data) => {
+    if(err)
+        throw err;
+    words = data.split("\n");
+})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('static'));
 
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.post('/contact', (req, res) => {
-    let firstname = req.body.firstname;
-    let lastname = req.body.lastname;
-    let strasse = req.body.strasse;
-    let hausnummer = req.body.hausnummer;
-    let postleitzahl = req.body.postleitzahl;
-    let phone = req.body.phone;
-    let email = req.body.email;
-    if(!firstname || !lastname || !strasse || !hausnummer || !postleitzahl || !phone) 
-        sendErrorMessage(res, 10001, "Bitte fülle alle Felder aus.", "*")
-    else {
-        // validation
-        if(firstname.length <= 2)
-            sendErrorMessage(res, 10002, "Bitte gib deinen echten Vornamen an, damit wir wissen, wer du bist.", "firstname");
-        else if(lastname.length <= 2)
-            sendErrorMessage(res, 10003, "Bitte gib deinen ehcten Nachnamen an, damit wir wissen, wer du bist.", "lastname");
-        else if(strasse.length <= 3)
-            sendErrorMessage(res, 10004, "Bitte gib deine Straße ein, damit wir wissen wo du wohnst und die Suche vereinfachen können.", "strasse");
-        else if(isNaN(hausnummer.length) || hausnummer == 0)
-            sendErrorMessage(res, 10005, "Bitte gib deine Hausnummer als Zahl and, damit wir wissen, wo du wohnst und die Suche verbessern können.");
-        else if(phone <= 0 || !("" + phone).match(/\d{11}/))
-            sendErrorMessage(res, 10006, "Deine Telefonnummer sollte in der Form ZZZZZZZZZZZ sein.");
-        else {
+app.get('/word', (req, res) => {
+    res.send(JSON.stringify({
+        word: getRandomWord()
+    }))
+});
 
-            // put into DB
+app.post('/saveDrawing', (req, res) => {
+    // we get a drawing
+    let data = req.body.data;
+    let drawing = data.drawing;
+    if(drawing.length > 0) {
+        let drawingSize = data.drawingSize;
+        let word = data.word;
+        let drawingUUID = uuid(20);
+        let time = data.time;
+        // save drawing in db
+        database.insert({
+            drawingUUID, word, drawingSize, drawing, time
+        });
+        console.log("Hey! New drawing :) -> " + word);
+        // send next word back
+        res.send(JSON.stringify({
+            word: getRandomWord(),
+            status: 'success'
+        }));
+    }
+});
 
-
-        }  
-
-    } 
-    
-})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`App is listening on port ${port}`)
+    console.log(`App is listening on port ${port}`);
 });
 
 
@@ -59,4 +68,17 @@ function sendErrorMessage(res, code, text, el) {
             el: el
         }
     })
+}
+
+function uuid(length) {
+    let res = "";
+    let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    for(let i = 0; i < length; i++) {
+        res += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return res;
+}
+
+function getRandomWord() {
+    return words[Math.floor(Math.random() * words.length)];
 }
---
+++ static/script.js
@@ -0,0 +1,117 @@
+// DrawingAI
+
+// host
+const host = "http://127.0.0.1:3000";
+endpoint = en => host + "/" + en;
+
+
+let word;
+let time = 20;
+let path = [];
+let drawing = [];
+let isDrawing = false;
+let drawingSize; 
+let tick = 0;
+let timeDOM;
+
+function preload() {
+    // get the words from the API
+    fetch(endpoint('word'))
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setWord(data.word);
+        });
+}
+
+function setup() {
+    drawingSize = (innerWidth > 500 ? 500 : innerWidth);
+    let margin = (innerWidth - drawingSize) / 2 + "px";
+    canvas = createCanvas(drawingSize, drawingSize);
+    canvas.mousePressed(startDrawing);
+    canvas.mouseReleased(endDrawing);
+    canvas.parent('sketch');
+    $('#sketch').css('margin-left', margin);
+}
+
+function draw() {
+    background('#23243A'); 
+    if(isDrawing) {
+        path.push({
+            x: mouseX,
+            y: mouseY
+        });
+    }
+    stroke(255);
+    strokeWeight(8);
+    noFill();
+    for(let i = 0; i < drawing.length; i++) {
+        let currentPath = drawing[i];
+        beginShape();
+        for(let j = 0; j < currentPath.length; j++) {
+            vertex(currentPath[j].x, currentPath[j].y);
+        }
+        endShape();
+    }
+    if(mouseX <= 0 || mouseY <= 0 || mouseX >= drawingSize || mouseY >= drawingSize)
+        endDrawing();
+    tick++;
+    // every second
+    if((tick % 30) == 0) {
+        time--;
+        if(time == 0) {
+            savePainting();
+        }
+        // update time
+        $('#time').html(time + "s" + " | " + word);
+    }
+}
+
+function startDrawing() {
+    isDrawing = true;
+    path = [];
+    drawing.push(path);
+}
+
+function endDrawing() {
+    isDrawing = false;
+}
+
+function savePainting() {
+    // if time = 0 or if user presses next
+    // send data to backend
+    fetch(endpoint('saveDrawing'), {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            data: { drawing, drawingSize, word, time }
+        })
+    })
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            // we get a new word back
+            setWord(data.word);
+            // delete the drawing
+            deleteDrawing();
+            // reset timer
+            resetTimer();
+        })
+}
+
+function deleteDrawing() {
+    drawing = [];
+}
+
+function resetTimer() {
+    time = 21;
+}
+
+function setWord(newWord) {
+    word = newWord;
+}
